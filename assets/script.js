b1f61f3347742d4b4fb99d59367b328715c4ce42
Matthew Courtney <matthewdc182@gmail.com>
2020-08-12 15:22:13 -0400

    created a bunch of psudo code in comments to help plan the logic for the game.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var startBtn = document.getElementById("startGame");
 var questionsEl = document.querySelector(".hide");
 var homepageEl = document.getElementById("homepage");
+var timerEl = document.getElementById("timer");
 
 
 function runGame() {
@@ -9,5 +10,27 @@ function runGame() {
     //display hide
     homepageEl.style.display = "none";
     questionsEl.style.display = "block";
+
+    //runTimer()
+    //generateQuestions()
     
 }
+
+//runtimer function
+    //set timer to 45 seconds
+    //if timer is done end game
+
+//generate questions function
+    //create a questions array with objects within the array
+        //include question, choices(array), and answer value(index)
+
+//check answers function
+    //if the answer is correct run nextQuestion
+    //if its incorrect -5 sec then run nextQuestion
+
+//nextquestion function 
+    //questionindex ++
+
+//end game function
+    //taks you to highscores page
+    //aska for name and logs high score to local storage
