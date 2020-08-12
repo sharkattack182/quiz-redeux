5fc09834537c2acb333283d2df48d539919bc791
Matthew Courtney <matthewdc182@gmail.com>
2020-08-12 12:06:07 -0400

    added the display styling to toggle between homepage and questions on the start button click

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var startBtn = document.getElementById("startGame");
+var questionsEl = document.querySelector(".hide");
+var homepageEl = document.getElementById("homepage");
+
+
+function runGame() {
+    //start a timer
+    //hide the homepage
+    //display hide
+    homepageEl.style.display = "none";
+    questionsEl.style.display = "block";
+    
+}
