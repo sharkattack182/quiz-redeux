a2928169cbe57e5757333028911c691eb1bc0c38
Matthew Courtney <matthewdc182@gmail.com>
2020-08-13 01:39:53 -0400

    got the run timer function working for 45 seconds and runs when the start button is pressed

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var questionsEl = document.querySelector(".hide");
 var homepageEl = document.getElementById("homepage");
 var timerEl = document.getElementById("timer");
 
+var secondsLeft = 45;
 
 function runGame() {
     //start a timer
@@ -10,16 +11,25 @@ function runGame() {
     //display hide
     homepageEl.style.display = "none";
     questionsEl.style.display = "block";
-
+    runTimer();
     //runTimer()
     //generateQuestions()
-    
+
 }
 
 //runtimer function
-    //set timer to 45 seconds
-    //if timer is done end game
+//set timer to 45 seconds
+//if timer is done end game
 
+function runTimer() {
+    var timer = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+        if (secondsLeft == 0) {
+            clearInterval(timer)
+        }
+    }, 1000);
+}
 //generate questions function
     //create a questions array with objects within the array
         //include question, choices(array), and answer value(index)
